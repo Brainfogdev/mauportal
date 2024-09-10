75ee42bdcc9e011f78673a85655f6c6a58518963
itzarbukhan <arbaazkhan1504@gmail.com>
2024-09-10 18:22:36 +0530

    10/09/24 Latest Evening Changes

--- next.config.js
+++ next.config.js
@@ -1,23 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config) => {
-      config.module.rules.push({
-        test: /\.(woff|woff2|eot|ttf|otf)$/, // Add file types to test for
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[hash].[ext]', // Set the name format for font files
-              outputPath: 'static/fonts/', // Specify the output directory
-              publicPath: '/_next/static/fonts/', // Adjust the public path for Next.js
-            },
+  async rewrites() {
+    return [
+      {
+        source: '/*',
+        destination: 'http://150.129.118.10:5000/*',
+      },
+    ];
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(woff|woff2|eot|ttf|otf)$/, // Add file types to test for
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[hash].[ext]', // Set the name format for font files
+            outputPath: 'static/fonts/', // Specify the output directory
+            publicPath: '/_next/static/fonts/', // Adjust the public path for Next.js
           },
-        ],
-      });
-  
-      return config;
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+        },
+      ],
+    });
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
